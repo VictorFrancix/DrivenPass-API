7c7929bc851c6462b39e041002e7b35b4b81bca7
Luana Lessa <lessalsn@gmail.com>
2022-08-02 13:56:31 -0300

    Code review

--- src/schemas/notesSchema.ts
+++
@@ -1,6 +0,0 @@
-import Joi from "joi";
-
-export const notesSchema = Joi.object().keys({
-    title: Joi.string().max(50).required(),
-    body: Joi.string().max(1000).required()
-});
--- src/schemas/wifiSchema.ts
+++
@@ -1,7 +0,0 @@
-import Joi from "joi";
-
-export const wifiSchema = Joi.object().keys({
-    name : Joi.string().required(),
-    title : Joi.string().required(),
-    password : Joi.string().required()
-});
